a9af0d2c89ef6bb63e3fb623820f2c698e47cf07
Brandon <brandu.tan@gmail.com>
2021-09-22 22:18:30 +0800

    SPM-33 updated docstring for swagger

--- restful_api/myapi/api/resources/official_enroll.py
+++ restful_api/myapi/api/resources/official_enroll.py
@@ -7,34 +7,111 @@ from myapi.extensions import db
 from myapi.models import OfficialEnroll
 
 class OfficialEnrollResource(Resource):
+  """CRUD Operations on official enrollments
 
-    method_decorators = [jwt_required()]
+    ---
+    get:
+      tags:
+        - api
+      responses:
+        200:
+          content:
+            application/json:
+              schema:
+                allOf:
+                  - $ref: '#/components/schemas/PaginatedResult'
+                  - type: object
+                    properties:
+                      results:
+                        type: array
+                        items:
+                          $ref: '#/components/schemas/OfficialEnrollSchema'
+    post:
+      tags:
+        - api
+      requestBody:
+        content:
+          application/json:
+            schema:
+              OfficialEnrollSchema
+      responses:
+        201:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  msg:
+                    type: string
+                    example: Enrollment record created
+                  user: OfficialEnrollSchema
+    put:
+      tags:
+        - api
+      requestBody:
+        content:
+          application/json:
+            schema:
+              OfficialEnrollSchema
+      responses:
+        201:
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  msg:
+                    type: string
+                    example: Enrollment passed record updated
+                  user: OfficialEnrollSchema
+    """
     
-    def __init__(self):
-      self.schema = OfficialEnrollSchema()
-      
-    def get(self, eng_id, course_id):
-      enrollment_record = OfficialEnroll.query.get_or_404(eng_id, course_id)
-      return {"employee": self.schema.dump(enrollment_record)}
-      
-    def post(self, eng_id, course_id, start_date, end_date):
-      new_enrollment = OfficialEnroll(eng_id, course_id, start_date, end_date)
-      
-      db.session.add(new_enrollment)
-      db.session.commit()
-      
-      return {"msg": "enrollment created", "enrollment": self.schema.dump(new_enrollment)}, 201
+  method_decorators = [jwt_required()]
+  
+  def __init__(self):
+    self.schema = OfficialEnrollSchema()
     
-    def put(self, eng_id, course_id, has_passed):
-      enrollment_record = OfficialEnroll.query.get_or_404(eng_id, course_id)
-      enrollment_record.has_passed = has_passed
-      db.session.commit()
-      
-      return {"msg": "enrollment created", "enrollment": self.schema.dump(enrollment_record)}, 201
+  def get(self, eng_id, course_id):
+    enrollment_record = OfficialEnroll.query.get_or_404(eng_id, course_id)
+    return {"employee": self.schema.dump(enrollment_record)}
+    
+  def post(self, eng_id, course_id, start_date, end_date):
+    new_enrollment = OfficialEnroll(eng_id, course_id, start_date, end_date)
+    
+    db.session.add(new_enrollment)
+    db.session.commit()
+    
+    return {"msg": "enrollment created", "enrollment": self.schema.dump(new_enrollment)}, 201
+  
+  def put(self, eng_id, course_id, has_passed):
+    enrollment_record = OfficialEnroll.query.get_or_404(eng_id, course_id)
+    enrollment_record.has_passed = has_passed
+    db.session.commit()
+    
+    return {"msg": "enrollment created", "enrollment": self.schema.dump(enrollment_record)}, 201
       
 
 class OfficialEnrollResourceList(Resource):
+    """Get all officially enrolled courses based on engineer id
 
+    ---
+    get:
+      tags:
+        - api
+      responses:
+        200:
+          content:
+            application/json:
+              schema:
+                allOf:
+                  - $ref: '#/components/schemas/PaginatedResult'
+                  - type: object
+                    properties:
+                      results:
+                        type: array
+                        items:
+                          $ref: '#/components/schemas/OfficialEnrollSchema'
+    """
     method_decorators = [jwt_required()]
 
     def __init__(self):
