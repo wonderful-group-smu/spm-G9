16e2977ebf202bda651671ae547ce362a8d57fd4
Edwin Lim <edwinlzs97@gmail.com>
2021-09-23 00:35:07 +0800

    completed CreateCourse form basic functional setup

--- frontend/src/Pages/CreateCourse/CreateCourse.js
+++ frontend/src/Pages/CreateCourse/CreateCourse.js
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import { array } from 'prop-types'
 import '../Pagelayout.css'
 import './CreateCourse.css'
 
 const CreateCourse = (props) => {
-  const handleSubmit = (contents) => {
-    console.log(contents);
-    contents.preventDefault();
+  const [courseName, setCourseName] = useState("");
+  const [trainer, setTrainer] = useState("");
+  const [desc, setDesc] = useState("");
+  const [prereqs, setPrereqs] = useState([]);
+
+  const addPrereq = (event) => {
+    setPrereqs([
+      ...prereqs,
+      event.target.value,
+    ]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    alert(`Submitted ${courseName}, ${trainer}, ${desc}, ${prereqs}`)
   }
 
   return (
@@ -16,32 +30,49 @@ const CreateCourse = (props) => {
       </div>
 
       <form onSubmit={handleSubmit}>
-        <label htmlFor="inputCourseTitle" className="form-label">Course Name</label>
-        <input className="form-control" id="inputCourseTitle" placeholder="Input Course Name..."/>
+        <label htmlFor="inputCourseName" className="form-label">Course Name</label>
+        <input
+          className="form-control"
+          id="inputCourseName"
+          placeholder="Input Course Name..."
+          onChange={e => setCourseName(e.target.value)}
+        />
 
         <label htmlFor="inputTrainer" className="form-label">Trainer</label>
-        <input className="form-control" list="trainerDatalist" id="inputTrainer" placeholder="Search..."/>
+        <input
+          className="form-control"
+          list="trainerDatalist"
+          id="inputTrainer"
+          placeholder="Search..."
+          onChange={e => setTrainer(e.target.value)}
+        />
         <datalist id="trainerDatalist">
           {props.trainer.map((trainer, i) => (
-            <option value={trainer} key={i}/>
+            <option value={trainer} key={i} />
           ))}
         </datalist>
 
-        <label htmlFor="inputDescription" className="form-label">Description</label>
-        <textarea className="form-control" id="inputDescription" placeholder="Input Course Description..." rows="3"/>
+        <label htmlFor="inputDesc" className="form-label">Description</label>
+        <textarea
+          className="form-control"
+          id="inputDesc"
+          placeholder="Input Course Description..."
+          rows="3"
+          onChange={e => setDesc(e.target.value)}
+        />
 
         <label htmlFor="form-check" className="form-label">Prerequisites</label>
-        <div className="form-check">  
-          {props.prerequisites.map((prereq, i) => (
+        <div className="form-check" onChange={addPrereq}>
+          {props.prereqs.map((prereq, i) => (
             <div key={i}>
-              <input className="form-check-input" type="checkbox" value={i} id="inputPrerequisites"/>
-              <label htmlFor="inputPrerequisites" className="form-check-label" >{prereq}</label>
+              <input className="form-check-input" type="checkbox" value={prereq} id="inputPrereq" />
+              <label htmlFor="inputPrereq" className="form-check-label" >{prereq}</label>
             </div>
           ))}
         </div>
 
         <button type="submit" className="btn btn-secondary">Create Course</button>
-        
+
       </form>
     </div>
   )
@@ -49,12 +80,12 @@ const CreateCourse = (props) => {
 
 CreateCourse.defaultProps = {
   trainer: ["Daniel Lim", "Brock Place", "Misty Holder"],
-  prerequisites: ["IS110", "IS111", "IS112", "IS113"],
+  prereqs: ["IS110", "IS111", "IS112", "IS113"],
 }
 
 CreateCourse.propTypes = {
   trainer: array,
-  prerequisites: array,
+  prereqs: array,
 }
 
 export default CreateCourse
