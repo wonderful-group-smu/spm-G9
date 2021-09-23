9fbd2eb0173ec76946918a0e9a63fb5622cf17ff
yarkhinephyo <yarkhinephyo@gmail.com>
2021-09-23 23:35:49 +0800

    SPM-73 Refactor flask myapi init

--- restful_api/myapi/manage.py
+++ restful_api/myapi/manage.py
@@ -13,28 +13,28 @@ def init():
     from myapi.extensions import db
     from myapi.models import User, Course, Prereq, Employee
 
+    items_to_add = []
     click.echo("create user")
     user = User(id=1, username="test", email="test@mail.com", password="testpassword", active=True)
-    db.session.add(user)
-    db.session.commit()
+    items_to_add.append(user)
     click.echo("created user")
     click.echo("create employee")
     employee_one = Employee(id=1, name="test", user_type="ENG")
-    db.session.add(employee_one)
-    db.session.commit()
+    items_to_add.append(employee_one)
     click.echo("created employee")
     click.echo("create courses")
     course_one = Course(course_id=1, description="course one description", name="course one name")
     course_two = Course(course_id=2, description="course two description", name="course two name")
-    db.session.add(course_one)
-    db.session.add(course_two)
-    db.session.commit()
+    items_to_add.append(course_one)
+    items_to_add.append(course_two)
     click.echo("created courses")
     click.echo("create prereq")
     prereq_one = Prereq(course_id=2, prereq_id=1)
-    db.session.add(prereq_one)
-    db.session.commit()
+    items_to_add.append(prereq_one)
     click.echo("created prereq")
+
+    db.session.add_all(items_to_add)
+    db.session.commit()
 
 
 if __name__ == "__main__":
