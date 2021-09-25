22cca2113f2e5d97dbfd1cdd207be25e06ed1b94
Joel Yang <joelytj@gmail.com>
2021-09-26 00:15:22 +0800

    Add unique constraint to self enroll table

--- restful_api/myapi/models/self_enroll.py
+++ restful_api/myapi/models/self_enroll.py
@@ -4,7 +4,8 @@ class SelfEnroll(db.Model):
     """Basic Self Enroll model"""
 
     __tablename__ = "self_enroll"
-    
+    __table_args__ = (db.UniqueConstraint('eng_id','course_id', name = 'unique_eng_self_enrolled_course'),)
+
     eng_id = db.Column(db.Integer, db.ForeignKey('employee.id'), primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), primary_key=True)
     
