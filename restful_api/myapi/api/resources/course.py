944af352e6030cc576ec4681d7c3b65e737c8444
yarkhinephyo <yarkhinephyo@gmail.com>
2021-09-26 22:27:05 +0800

    SPM-79 Add back auth

--- restful_api/myapi/api/resources/course.py
+++ restful_api/myapi/api/resources/course.py
@@ -60,7 +60,7 @@ class CourseResource(Resource):
                 properties:
                   message: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
     """
-    # method_decorators = [jwt_required()]
+    method_decorators = [jwt_required()]
 
     def get(self, course_id):
         try:
