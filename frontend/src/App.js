6a9f91f746e71910592ef185a1299469863fc627
xiulingchua <51019711+xiulingchua@users.noreply.github.com>
2021-09-26 23:08:32 +0800

    Enrolment Request (#16)
    Completed enrolment request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,10 @@ import React, { useState } from 'react'
 import Navbar from './Components/Navbars/Navbar'
 import Courses from './Pages/Courses/Courses'
 import Records from './Pages/Records/Records'
+import EnrolmentRequest from './Pages/EnrolmentRequest/EnrolmentRequest'
 import Enrolled from './Pages/Enrolled/Enrolled'
 import CreateCourse from './Pages/CreateCourse/CreateCourse'
 import SelfEnrollmentForm from './Pages/SelfEnrollmentForm/SelfEnrollmentForm'
-
 import TopNav from './Components/Navbars/TopNav/TopNav'
 import Home from './Pages/Home/Home'
 
@@ -39,6 +39,7 @@ function App() {
             <TopNav />
             <div className='content'>
             <Switch>
+              <Route path='/enrolmentrequest' component={EnrolmentRequest} />
               <Route path='/courses/selfenrollmentform' component={SelfEnrollmentForm} />
               <Route path='/courses' component={Courses} />
               <Route path='/records' component={Records} />
