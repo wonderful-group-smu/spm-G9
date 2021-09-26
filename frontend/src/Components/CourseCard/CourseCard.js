0e570691fe4341fb09a1a279e799b8110fe7a2ff
Edwin Lim <edwinlzs97@gmail.com>
2021-09-26 21:06:24 +0800

    80% done on FE delete course functionality

--- frontend/src/Components/CourseCard/CourseCard.js
+++ frontend/src/Components/CourseCard/CourseCard.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CourseCard.css'
 import * as Si from 'react-icons/si'
 import * as Bs from 'react-icons/bs'
 import { Link } from 'react-router-dom'
-import { bool, string } from 'prop-types'
+import { array, bool, func, string } from 'prop-types'
 
 const CourseCard = (props) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleClickSelect = (event) => {
+    setSelected(!selected);
+    // if (selected) {
+    //   props.setDeleteList([...props.deleteList, event.target]);
+    // }
+    event.preventDefault();
+  }
+
   return (
-  <>
-   <Link to='/CourseTitle' className='try' style={{ textDecoration: 'none' }}
-    onClick={props.deleteMode ? "" : (event) => event.preventDefault()}>
-      <div className='col-sm shadow'>
-        <div className='card hover-backgroundcolor'>
-          <div className='card-body'>
-            <h5 className='card-title'>
-              <Si.SiGoogleclassroom />
-              &nbsp;{props.cardTitle}
-            </h5>
-            <p className='card-subtitle mb-2 text-muted'>{props.cardText}</p>
-            <p className='card-text'>
-              <Bs.BsPeopleCircle size={25} />
-              &nbsp;{props.trainer}
-            </p>
+    <>
+      <Link to='/CourseTitle' className='try' style={{ textDecoration: 'none' }}
+        onClick={props.deleteMode ? handleClickSelect : ""}>
+        <div className='col-sm shadow'>
+          <div className='card hover-backgroundcolor'>
+            <div className={selected && props.deleteMode ? 'card-body selected-body' : 'card-body'}>
+              <h5 className='card-title'>
+                <Si.SiGoogleclassroom />
+                &nbsp;{props.cardTitle}
+              </h5>
+              <p className='card-subtitle mb-2 text-muted'>{props.cardText}</p>
+              <p className='card-text'>
+                <Bs.BsPeopleCircle size={25} />
+                &nbsp;{props.trainer}
+                <Bs.BsCircle className="checkbox" hidden={selected || !props.deleteMode} />
+                <Bs.BsCheckCircle className="checkbox selected" hidden={!selected || !props.deleteMode} />
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
 
-  </>
-   
+    </>
+
   )
 }
 
@@ -38,6 +50,7 @@ CourseCard.defaultProps = {
   trainer: 'Daniel Lim (Senior Engineer)',
   selected: false,
   deleteMode: false,
+  
   // age: "45"
 }
 
@@ -47,6 +60,8 @@ CourseCard.propTypes = {
   trainer: string,
   selected: bool,
   deleteMode: bool,
+  deleteList: array,
+  setDeleteList: func,
 }
 
 export default CourseCard
--- frontend/src/Pages/Courses/Courses.js
+++ frontend/src/Pages/Courses/Courses.js
@@ -5,33 +5,83 @@ import './Courses.css'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+const CourseData = [
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+  {
+    cardTitle: "IS110: Python Programming",
+    cardText: "Starts on 12 Jan 2021, End on 12 Mar 2021",
+    instructor: "Daniel Lim (Senior Engineer)",
+  },
+]
+
 const Courses = () => {
+  const [pageTitle, setPageTitle] = useState("Courses")
   const [deleteMode, setDeleteMode] = useState(false)
+  
+  const [deleteList, setDeleteList] = useState([]);
 
   const handleDeleteMode = () => {
+    setPageTitle(deleteMode ? "Courses" : "Delete Course" )
     setDeleteMode(!deleteMode);
-    console.log(deleteMode);
   };
+
+  // const handleDelete = () => {
+
+  // }
+
+  // const check = () => {
+  //   console.log("check deleteMode: ", deleteMode);
+  //   console.log("check deleteList: ", deleteList);
+  // }
 
   return (
     <div id='pagelayout'>
+      {/* <button onClick={check}>Check me</button> */}
 
       <div id='section-header'>
-        <h5 id='page-title'>Courses</h5>
+        <h5 id='page-title'>{pageTitle}</h5>
         <Link to='/createcourse'>
-          <button className="btn btn-secondary">Create a Course</button>
+          <button hidden={deleteMode} className="btn btn-secondary">Create a Course</button>
         </Link>
-        <button className="btn btn-secondary" onClick={handleDeleteMode}>Delete Courses</button>
+        <button hidden={deleteMode} className="btn btn-secondary" onClick={handleDeleteMode}>Delete Courses</button>
+        <button hidden={!deleteMode} className="btn btn-secondary">Delete Selected Courses</button>
+        <button hidden={!deleteMode} className="btn btn-secondary" onClick={handleDeleteMode}>Exit</button>
       </div>
 
       <div className='row' >
         <div>
-          {[0, 1, 2, 3, 4, 5, 6].map((i) => (
+          {CourseData.map((data, i) => (
             <CourseCard key={{ i }}
-              cardTitle="IS110: Python Programming"
-              cardText="Starts on 12 Jan 2021, End on 12 Mar 2021"
-              instructor="Daniel Lim (Senior Engineer)"
+              cardTitle={data.cardTitle}
+              cardText={data.cardText}
+              instructor={data.instructor}
               deleteMode={deleteMode}
+              deleteList={deleteList}
+              setDeleteList={setDeleteList}
             />
           ))}
 
@@ -40,5 +90,4 @@ const Courses = () => {
     </div>
   )
 }
-
 export default Courses
