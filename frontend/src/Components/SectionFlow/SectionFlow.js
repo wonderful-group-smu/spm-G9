7c0adebd7f218bb0c1e56572d8ec84ab8693d2cc
Edwin Lim <edwinlzs97@gmail.com>
2021-10-10 13:45:26 +0800

    added create section functionality, able to reorder sections

--- frontend/src/Components/SectionFlow/SectionFlow.js
+++ frontend/src/Components/SectionFlow/SectionFlow.js
@@ -1,27 +1,60 @@
+import { useState } from 'react'
 import { array } from 'prop-types'
 import * as Vsc from 'react-icons/vsc'
 import './SectionFlow.css'
 
 const SectionFlow = (props) => {
+    const [sections, setSections] = useState(props.sections)
+    const [dragTitle, setDragTitle] = useState();
+
+    const handleDrag = (e) => {
+        setDragTitle(e.currentTarget.title);
+        console.log(e.currentTarget)
+    };
+
+    const handleDrop = (e) => {
+        const dragSection = sections.find((section) => section.title === dragTitle);
+        const dropSection = sections.find((section) => section.title === e.currentTarget.title);
+
+        const dragSectionOrder = dragSection.order;
+        const dropSectionOrder = dropSection.order;
+
+        const newSections = sections.map((section) => {
+            if (section.title === dragTitle) {
+                section.order = dropSectionOrder;
+            }
+            if (section.title === e.currentTarget.title) {
+                section.order = dragSectionOrder;
+            }
+            return section;
+        });
+        setSections(newSections)
+    }
 
     return (
         <div className='column'>
             {
-                props.sections.map((title, i) => (
-                    i == 0 ?
-                        <>
-                            <button key={i} type="button" disabled id="section-btn" className="btn btn-outline-primary">
-                                {title}
-                            </button>
-                        </>
-                        :
-                        <>
-                            <Vsc.VscArrowRight className="section-link" />
-                            <button key={i} type="button" disabled id="section-btn" className="btn btn-outline-primary">
-                                {title}
-                            </button>
-                        </>
+                sections
+                    .sort((a, b) => a.order - b.order)
+                    .map((section, i) => (
+                    <>
+                        {i == 0 ? "" : <Vsc.VscArrowRight className="section-link" />}
+                        <button key={i}
+                            type="button"
+                            title={section.title}
+                            // disabled
+                            id="section-btn"
+                            className="btn btn-outline-primary"
+                            draggable={true}
+                            onDragOver={(ev) => ev.preventDefault()}
+                            onDragStart={handleDrag}
+                            onDrop={handleDrop}
+                        >
+                            {section.title}
+                        </button>
+                    </>
                 ))
+                
             }
         </div>
     )
@@ -29,10 +62,22 @@ const SectionFlow = (props) => {
 
 SectionFlow.defaultProps = {
     sections: [
-        "Variables & Operators",
-        "Conditional Statements & Loops",
-        "Lists, Tuples & Dictionaries",
-        "File Read & Write"
+        {
+            title: "Variables & Operators",
+            order: 1
+        },
+        {
+            title: "Conditional Statements & Loops",
+            order: 2
+        },
+        {
+            title: "Lists, Tuples & Dictionaries",
+            order: 3
+        },
+        {
+            title: "File Read & Write",
+            order: 4
+        },
     ]
 }
 
