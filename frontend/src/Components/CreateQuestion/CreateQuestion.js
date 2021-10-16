aff562c345f2f57baffb9f1b727fc143c2aa3e2b
Edwin Lim <edwinlzs97@gmail.com>
2021-10-17 00:59:28 +0800

    completed CreateQuiz UI, need cleanup duplicate code

--- frontend/src/Components/CreateQuestion/CreateQuestion.js
+++ frontend/src/Components/CreateQuestion/CreateQuestion.js
@@ -1,8 +1,11 @@
-import { bool, number, string } from "prop-types";
+import { useState } from 'react';
+import { object } from "prop-types";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import './CreateQuestion.css';
-// import { useState } from "react";
 
-const CreateQuestion = () => {
+const CreateQuestion = (props) => {
+    const [questionType, setQuestionType] = useState(true)
+    const question_id = props.question.question_id
 
     const changeQuestion = (e) => {
         // setQuestion({
@@ -13,10 +16,19 @@ const CreateQuestion = () => {
         return (e)
     }
 
+    const changeQuestionType = (e) => {
+        if (e.target.id == 'question-type-mcq') {
+            setQuestionType(true);
+        }
+        else if (e.target.id == 'question-type-tf') {
+            setQuestionType(false);
+        }
+    }
+
     return (
         <div className="create-question-container">
             <div className="question">
-                <h6>Question Description</h6>
+                <h6>{question_id}) Question Description</h6>
                 <textarea
                     className="form-control"
                     id="inputQuestionDesc"
@@ -26,67 +38,118 @@ const CreateQuestion = () => {
                 />
                 <div className="form-check" id="question-type">
                     <label className="form-check-label" htmlFor="question-type-mcq">
-                        MCQ<input className="form-check-input" type="radio" name="question-type" id="question-type-mcq" />
+                        MCQ<input className="form-check-input" type="radio"
+                            name={"question-type-" + question_id} id="question-type-mcq" onClick={changeQuestionType} />
                     </label>
                 </div>
                 <div className="form-check" id="question-type">
                     <label className="form-check-label" htmlFor="question-type-tf">
-                        True/False<input className="form-check-input" type="radio" name="question-type" id="question-type-tf" />
+                        True/False<input className="form-check-input" type="radio"
+                            name={"question-type-" + question_id} id="question-type-tf" onClick={changeQuestionType} />
                     </label>
                 </div>
             </div>
 
             <div className="answer-options-container">
-                <h6>Options</h6>
+                <h6>{questionType ? "Options" : "Correct Answer"}</h6>
                 <div className="answer-options">
-                    <div className="input-group">
-                        <span className="input-group-text" id="option-a">
-                            a)
-                        </span>
-                        <input
-                            type="text" className="form-control answer-option"
-                            placeholder="Input Option A..." aria-label="option-a" aria-describedby="option-a"
-                        />
-                        <div className="form-check" id="correct-answer">
-                            <input className="form-check-input" type="radio" name="correct-answer" id="answer-a" />
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <span className="input-group-text" id="option-b">
-                            b)
-                        </span>
-                        <input
-                            type="text" className="form-control answer-option"
-                            placeholder="Input Option B..." aria-label="option-a" aria-describedby="option-b"
-                        />
-                        <div className="form-check" id="correct-answer">
-                            <input className="form-check-input" type="radio" name="correct-answer" id="answer-b" />
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <span className="input-group-text" id="option-c">
-                            c)
-                        </span>
-                        <input
-                            type="text" className="form-control answer-option"
-                            placeholder="Input Option C..." aria-label="option-a" aria-describedby="option-c"
-                        />
-                        <div className="form-check" id="correct-answer">
-                            <input className="form-check-input" type="radio" name="correct-answer" id="answer-c" />
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <span className="input-group-text" id="option-c">
-                            d)
-                        </span>
-                        <input
-                            type="text" className="form-control answer-option"
-                            placeholder="Input Option D..." aria-label="option-a" aria-describedby="option-c"
-                        />
-                        <div className="form-check" id="correct-answer">
-                            <input className="form-check-input" type="radio" name="correct-answer" id="answer-d" />
-                        </div>
-                    </div>
+                    {questionType ?
+                        <>
+                            <div className="input-group">
+                                <span className="input-group-text" id="option-a">
+                                    A)
+                                </span>
+                                <input
+                                    type="text" className="form-control answer-option"
+                                    placeholder="Input Option A..." aria-label="option-a" aria-describedby="option-a"
+                                />
+                                <div className="form-check" id="correct-answer">
+                                    <OverlayTrigger placement="top" overlay={
+                                        <Tooltip>
+                                            Set A as correct answer
+                                        </Tooltip>
+                                    }>
+                                        <input className="form-check-input" type="radio"
+                                            name={"correct-answer-" + question_id} id="answer-a"
+                                        />
+                                    </OverlayTrigger>
+                                </div>
+                            </div>
+                            <div className="input-group">
+                                <span className="input-group-text" id="option-b">
+                                    B)
+                                </span>
+                                <input
+                                    type="text" className="form-control answer-option"
+                                    placeholder="Input Option B..." aria-label="option-a" aria-describedby="option-b"
+                                />
+                                <div className="form-check" id="correct-answer">
+                                    <OverlayTrigger placement="top" overlay={
+                                        <Tooltip>
+                                            Set B as correct answer
+                                        </Tooltip>
+                                    }>
+                                        <input className="form-check-input" type="radio"
+                                            name={"correct-answer-" + question_id} id="answer-b" />
+                                    </OverlayTrigger>
+                                </div>
+                            </div>
+                            <div className="input-group">
+                                <span className="input-group-text" id="option-c">
+                                    C)
+                                </span>
+                                <input
+                                    type="text" className="form-control answer-option"
+                                    placeholder="Input Option C..." aria-label="option-a" aria-describedby="option-c"
+                                />
+                                <div className="form-check" id="correct-answer">
+                                    <OverlayTrigger placement="top" overlay={
+                                        <Tooltip>
+                                            Set C as correct answer
+                                        </Tooltip>
+                                    }>
+                                        <input className="form-check-input" type="radio"
+                                            name={"correct-answer-" + question_id} id="answer-c" />
+                                    </OverlayTrigger>
+                                </div>
+                            </div>
+                            <div className="input-group">
+                                <span className="input-group-text" id="option-c">
+                                    D)
+                                </span>
+                                <input
+                                    type="text" className="form-control answer-option"
+                                    placeholder="Input Option D..." aria-label="option-a" aria-describedby="option-c"
+                                />
+                                <div className="form-check" id="correct-answer">
+                                    <OverlayTrigger placement="top" overlay={
+                                        <Tooltip>
+                                            Set D as correct answer
+                                        </Tooltip>
+                                    }>
+                                        <input className="form-check-input" type="radio"
+                                        name={"correct-answer-" + question_id} id="answer-d" />
+                                    </OverlayTrigger>
+                                </div>
+                            </div>
+
+                        </>
+                        :
+                        <>
+                            <div className="form-check" id="correct-answer-tf">
+                                <label className="form-check-label" htmlFor="correct-answer-true">
+                                    True<input className="form-check-input" type="radio"
+                                        name={"correct-answer-" + question_id} id="correct-answer-true" />
+                                </label>
+                            </div>
+                            <div className="form-check" id="correct-answer-tf">
+                                <label className="form-check-label" htmlFor="correct-answer-false">
+                                    False<input className="form-check-input" type="radio"
+                                        name={"correct-answer-" + question_id} id="correct-answer-false" />
+                                </label>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </div>
@@ -94,9 +157,10 @@ const CreateQuestion = () => {
 }
 
 CreateQuestion.propTypes = {
-    question_id: number,
-    question_text: string,
-    question_type: bool,
+    // question_id: number,
+    // question_text: string,
+    // question_type: bool,
+    question: object,
 }
 
 export default CreateQuestion;
--- frontend/src/Pages/CreateSection/CreateSection.js
+++ frontend/src/Pages/CreateSection/CreateSection.js
@@ -3,21 +3,30 @@ import { useHistory } from "react-router-dom";
 import * as Bi from 'react-icons/bi';
 import { Accordion } from 'react-bootstrap'
 import CreateQuestion from '../../Components/CreateQuestion/CreateQuestion';
-import { bool, number, string } from "prop-types";
+import { array } from "prop-types";
 import '../Pagelayout.css';
 import './CreateSection.css';
 
 const CreateSection = (props) => {
   const [sectionTitle, setSectionTitle] = useState("");
   const [selectedFiles, setSelectedFiles] = useState([]);
+  
+  const [questionCount, setQuestionCount] = useState(1);
   const [questions, setQuestions] = useState(props.questionArr);
 
   let history = useHistory();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
     alert(`Submitted ${sectionTitle}, ${selectedFiles}`);
+  }
+
+  const addQuestion = () => {
+    let currentQuestions = questions;
+    currentQuestions.push(questionTemplate(questionCount + 1));
+    setQuestions(currentQuestions);
+    setQuestionCount(questionCount + 1);
   }
 
   return (
@@ -52,10 +61,19 @@ const CreateSection = (props) => {
             <Accordion.Header>Section Quiz</Accordion.Header>
             <Accordion.Body>
               {
-                questions.map((question, i) => (
-                  <CreateQuestion key={i} question={question} setQuestions={setQuestions}/>
-                ))
+                questions.map((question, i) => {
+                  console.log(i)
+                  return (
+                    <>
+                      <CreateQuestion key={i} question={question} setQuestions={setQuestions} />
+                      <hr />
+                    </>
+                  )
+                }
+                
+                )
               }
+              <button type="button" className="btn btn-outline-primary" onClick={addQuestion}>Add Question</button>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
@@ -66,24 +84,24 @@ const CreateSection = (props) => {
   )
 }
 
-CreateSection.defaultProps = {
-  questionArr: [
+const questionTemplate = (questionCount) => {
+  return (
     {
-      question_id: 1,
+      question_id: questionCount,
       question_text: "",
       question_type: true,
     }
+  )
+} 
+
+CreateSection.defaultProps = {
+  questionArr: [
+    questionTemplate(1),
   ]
 }
 
 CreateSection.propTypes = {
-  questionArr: [
-    {
-      question_id: number,
-      question_text: string,
-      question_type: bool,
-    }
-  ]
+  questionArr: array,
 }
 
 export default CreateSection;
