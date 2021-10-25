96127ee7f7c7bb6523c7324d1f9937fea3b081a6
xiulingchua <xiulingchua99@gmail.com>
2021-10-26 02:00:08 +0800

    Backend Integration
    - Class details information retrieved
    - Added in the self enrolled functionality

--- frontend/src/Apis/Api.js
+++ frontend/src/Apis/Api.js
@@ -4,34 +4,33 @@ import jwt from 'jwt-decode'
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/'
 
 const getAuthHeaders = () => {
-    let token = localStorage.getItem('token')
-    if (token === null) {
-        throw new Error('token not in')
-    }
-    const headers = {
-        'content-type': 'application/json',
-        'authorization': 'Bearer ' + token
-    }
-    return headers
+  let token = localStorage.getItem('token')
+  if (token === null) {
+    throw new Error('token not in')
+  }
+  const headers = {
+    'content-type': 'application/json',
+    authorization: 'Bearer ' + token,
+  }
+  return headers
 }
 
 const getEmployeeID = () => {
-    let token = localStorage.getItem('token')
-    const employeeID= jwt(token).sub
-    console.log(employeeID)
-    return employeeID
+  let token = localStorage.getItem('token')
+  const employeeID = jwt(token).sub
+  // console.log(employeeID)
+  return employeeID
 }
-    
 
 const login = (data) => axios.post(`${BASE_URL}auth/login`, data)
 
 const getCourseList = () => {
-    const headers = getAuthHeaders();
-    return axios.get(`${BASE_URL}api/v1/courses/0`, {headers});
+  const headers = getAuthHeaders()
+  return axios.get(`${BASE_URL}api/v1/courses/0`, { headers })
 }
-const getCourseClasses = ({course_id}) => {
-    const headers = getAuthHeaders();
-    return axios.get(`${BASE_URL}api/v1/course_classes/` + course_id, {headers})
+const getCourseClasses = ({ course_id }) => {
+  const headers = getAuthHeaders()
+  return axios.get(`${BASE_URL}api/v1/course_classes/` + course_id, { headers })
 }
 // const getClassSections = ({ course_id, trainer_id }) => {
 //     const headers = getAuthHeaders();
@@ -41,48 +40,105 @@ const getCourseClasses = ({course_id}) => {
 // }
 
 const addNewCourse = ({ course_id, name, description, prereqs }) => {
-    const headers = getAuthHeaders();
-    return axios.post(`${BASE_URL}api/v1/course/` + course_id, {
-        course_id, name, description, prereqs
-    }, {headers});
+  const headers = getAuthHeaders()
+  return axios.post(
+    `${BASE_URL}api/v1/course/` + course_id,
+    {
+      course_id,
+      name,
+      description,
+      prereqs,
+    },
+    { headers }
+  )
 }
 const addNewSection = ({ course_id, section_name, materials, trainer_id }) => {
-    const headers = getAuthHeaders();
-    return axios.post(`${BASE_URL}api/v1/class_section/0`, {
-        course_id, section_name, materials, trainer_id
-    }, {headers});
+  const headers = getAuthHeaders()
+  return axios.post(
+    `${BASE_URL}api/v1/class_section/0`,
+    {
+      course_id,
+      section_name,
+      materials,
+      trainer_id,
+    },
+    { headers }
+  )
 }
 const addNewQuiz = ({ course_id, section_id, trainer_id, is_graded }) => {
-    const headers = getAuthHeaders();
-    return axios.post(`${BASE_URL}api/v1/quiz/0`, {
-        course_id, section_id, trainer_id, is_graded
-    }, {headers});
+  const headers = getAuthHeaders()
+  return axios.post(
+    `${BASE_URL}api/v1/quiz/0`,
+    {
+      course_id,
+      section_id,
+      trainer_id,
+      is_graded,
+    },
+    { headers }
+  )
 }
 
 const getEnrolledList = () => {
-    const headers = getAuthHeaders();
-    const employeeID=getEmployeeID()
-    // console.log( axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, {headers}))
-    return axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, {headers});
+  const headers = getAuthHeaders()
+  const employeeID = getEmployeeID()
+  // console.log( axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, {headers}))
+  return axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, { headers })
 }
 
 const getClassDetails = (course_id, trainer_id) => {
-    const headers = getAuthHeaders();
-    // console.log( axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, {headers}))
-    return axios.get(`${BASE_URL}api/v1/course_class/${course_id}&${trainer_id}`, {headers});
+  const headers = getAuthHeaders()
+  // console.log( axios.get(`${BASE_URL}api/v1/enrollments/${employeeID}`, {headers}))
+  return axios.get(
+    `${BASE_URL}api/v1/course_class/${course_id}&${trainer_id}`,
+    { headers }
+  )
 }
+
+const addSelfEnroll = (course_id, trainer_id) => {
+  const employeeID = getEmployeeID()
+
+  const detail_input = {
+    eng_id: employeeID,
+    course_id: course_id,
+    trainer_id: trainer_id,
+    is_official: false,
+  }
+
+  const headers = getAuthHeaders()
+  return axios.post(
+    `${BASE_URL}api/v1/enroll/${employeeID}&${course_id}&${trainer_id}`,
+    detail_input,
+    { headers }
+  )
+}
+
+
+
+const getSelfEnroll = (course_id, trainer_id) => {
+  const employeeID = getEmployeeID()
+  const headers = getAuthHeaders()
+  return axios.get(
+    `${BASE_URL}api/v1/enroll/${employeeID}&${course_id}&${trainer_id}`,
+    
+    { headers }
+  )
+}
+
 
 
 
 export {
-    login,
-    getCourseList,
-    getCourseClasses,
-    // getClassSections,
-    addNewCourse,
-    addNewSection,
-    addNewQuiz,
-    getEmployeeID,
-    getEnrolledList,
-    getClassDetails
+  login,
+  getCourseList,
+  getCourseClasses,
+  // getClassSections,
+  addNewCourse,
+  addNewSection,
+  addNewQuiz,
+  getEmployeeID,
+  getEnrolledList,
+  getClassDetails,
+  addSelfEnroll,
+  getSelfEnroll
 }
