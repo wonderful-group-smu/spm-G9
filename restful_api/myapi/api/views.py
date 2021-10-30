d1f0499c81474f8fb72125ee33619e56d1fbb215
Joel Yang <joelytj@gmail.com>
2021-10-30 23:53:31 +0800

    Fix linter

--- restful_api/myapi/api/views.py
+++ restful_api/myapi/api/views.py
@@ -61,6 +61,7 @@ api.add_resource(QuizResource, "/quiz/<int:course_id>&<int:section_id>&<int:trai
 api.add_resource(QuizResourceList, "/quizzes/<int:course_id>&<int:trainer_id>", endpoint="quizzes")
 api.add_resource(QuizAttemptResource, "/quiz_attempt/<int:course_id>&<int:section_id>&<int:trainer_id>&<int:eng_id>", endpoint="quiz_attempt")
 
+
 @blueprint.before_app_first_request
 def register_views():
     """Return json error for marshmallow validation errors."""
